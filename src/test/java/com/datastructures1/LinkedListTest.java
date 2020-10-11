bad60419f1963dd2ad5ddde0dd0863a5b99c35fe
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-10-12 00:51:12 +0530

    [Kirti]Added:nodes added at end of list

--- src/test/java/com/datastructures1/LinkedListTest.java
+++ src/test/java/com/datastructures1/LinkedListTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 public class LinkedListTest {
 	@Test
-	public void given3NumberWhenAddedToLinkedListShouldBeAdded() {
+	public void given3NumberWhenAddedToLinkedListShouldBeAddedAtFront() {
 		Node<Integer> firstNode = new Node<Integer>(70);
 		Node<Integer> secondNode = new Node<Integer>(30);
 		Node<Integer> thirdNode = new Node<Integer>(56);
@@ -12,6 +12,18 @@ public class LinkedListTest {
 		list.addAtFront(firstNode);
 		list.addAtFront(secondNode);
 		list.addAtFront(thirdNode);
+		list.printList();
+	}
+	
+	@Test
+	public void given3NumberWhenAddedToLinkedListShouldBeAddedAtEnd() {
+		Node<Integer> firstNode = new Node<Integer>(56);
+		Node<Integer> secondNode = new Node<Integer>(30);
+		Node<Integer> thirdNode = new Node<Integer>(70);
+		MyLinkedList list = new MyLinkedList();
+		list.addAtEnd(firstNode);
+		list.addAtEnd(secondNode);
+		list.addAtEnd(thirdNode);
 		list.printList();
 	}
 }
