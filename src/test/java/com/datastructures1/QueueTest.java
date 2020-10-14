e9b1e26d61f8f0d8bc22b27e4bf398ab46495fc7
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-10-14 23:51:01 +0530

    [Kirti]:Added:dequeue used to pop element

--- src/test/java/com/datastructures1/QueueTest.java
+++ src/test/java/com/datastructures1/QueueTest.java
@@ -15,4 +15,19 @@ public class QueueTest {
 		queue.print();
 
 	}
+	
+	@Test
+	public void given3NumberWhenAddedToQueueShouldBeDequeue() {
+		Node<Integer> firstNode = new Node<Integer>(56);
+		Node<Integer> secondNode = new Node<Integer>(30);
+		Node<Integer> thirdNode = new Node<Integer>(70);
+		MyQueue queue = new MyQueue();
+		queue.enqueue(firstNode);
+		queue.enqueue(secondNode);
+		queue.enqueue(thirdNode);
+		queue.print();
+		queue.dequeue();
+		queue.print();
+
+	}
 }
